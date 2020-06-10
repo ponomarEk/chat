a5daf0c5aec7a8516c968bf7cc188fe442aa2483
Oleksandr Ponomarenko <ponomarek91@gmail.com>
2020-06-10 03:49:05 +0300

    Updates

---
+++ precache-manifest.3896f63b0fa578decbdca2933d175838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090e77101772cfb21d6f7a4def9b8873",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "c8aa9f92fb02c83b91eb",
+    "url": "/chat/static/css/main.68269fba.chunk.css"
+  },
+  {
+    "revision": "7aee87c0cc2a04e48e85",
+    "url": "/chat/static/js/2.7d3e8ddc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chat/static/js/2.7d3e8ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8aa9f92fb02c83b91eb",
+    "url": "/chat/static/js/main.175eb9c7.chunk.js"
+  },
+  {
+    "revision": "cdcebcbc55506001bdb7",
+    "url": "/chat/static/js/runtime-main.0cd3333a.js"
+  }
+]);
